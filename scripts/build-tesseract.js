50e7eeff3fc4fe20c1fb850758b932b69d4ae306
Simon Hofmann <mail@simon-hofmann.org>
2022-10-10 15:34:40 +0200

    Init submodules

--- scripts/build-tesseract.js
+++ scripts/build-tesseract.js
@@ -34,17 +34,13 @@ shell.echo(`Working directory: ${homeDir}`);
 
 // ------ submodules ------
 shell.echo('Initializing Git submodules.');
-if (shell.test('-e', 'tesseract') && shell.test('-e', 'leptonica')) {
-  shell.echo('The \'tesseract\' and \'leptonica\' directories already exist.')
-} else {
-  if (process.platform === 'win32') {
-    // https://stackoverflow.com/questions/49256190/how-to-fix-git-sh-setup-file-not-found-in-windows/50833818
-    shell.env['PATH'] = `${shell.env['PATH']};C:\\Program Files\\Git\\usr\\bin;C:\\Program Files\\Git\\mingw64\\libexec\\git-core`;
-  }
-  shell.echo('git submodule init start.')
-  shell.exec('git submodule update --init');
-  shell.echo('git submodule init end.')
+if (process.platform === 'win32') {
+  // https://stackoverflow.com/questions/49256190/how-to-fix-git-sh-setup-file-not-found-in-windows/50833818
+  shell.env['PATH'] = `${shell.env['PATH']};C:\\Program Files\\Git\\usr\\bin;C:\\Program Files\\Git\\mingw64\\libexec\\git-core`;
 }
+shell.echo('git submodule init start.')
+shell.exec('git submodule update --init');
+shell.echo('git submodule init end.')
 
 // ------ libraries ------
 buildLibjpeg ('libjpeg');
